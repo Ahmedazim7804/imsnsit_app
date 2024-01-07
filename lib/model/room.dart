b32e7d1261bd4668a35ea2b88f97e2e2430367c3
ahmedazim7804 <ahmedazim7804@gmail.com>
2024-01-07 11:55:16 +0530

    completed the room_screen

---
+++ lib/model/room.dart
@@ -0,0 +1,11 @@
+class Room {
+  const Room({required this.name, this.mon, this.tue, this.wed, this.thu, this.fri});
+
+  final String name;
+  final List<String>? mon;
+  final List<String>? tue;
+  final List<String>? wed;
+  final List<String>? thu;
+  final List<String>? fri;
+
+}
