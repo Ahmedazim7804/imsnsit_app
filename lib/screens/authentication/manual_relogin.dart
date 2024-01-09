46d036fe9109fd9da368961c1ed04381db1dfa6c
ahmedazim7804 <ahmedazim7804@gmail.com>
2024-01-09 15:27:58 +0530

    manual_login screen fixed

--- lib/screens/authentication/manual_relogin.dart
+++ lib/screens/authentication/manual_relogin.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:imsnsit/provider/ims_provider.dart';
+import 'package:loader_overlay/loader_overlay.dart';
 import 'package:provider/provider.dart';
 import 'package:imsnsit/model/functions.dart';
 import 'package:imsnsit/model/imsnsit.dart';
@@ -14,6 +15,8 @@ class ManualRelogin extends StatelessWidget {
 
   Future<void> tryRelogin(BuildContext context, Ims ims, TextEditingController controller) async {
 
+    context.loaderOverlay.show();
+
     String captchaText = controller.text;
     
     final SharedPreferences prefs = await SharedPreferences.getInstance();
@@ -21,6 +24,8 @@ class ManualRelogin extends StatelessWidget {
     String password = prefs.getString('password')!;
 
     await ims.authenticate(captchaText, username, password);
+
+    context.loaderOverlay.hide();
 
     if (ims.isAuthenticated) {
       context.go('/attandance');
@@ -36,43 +41,63 @@ class ManualRelogin extends StatelessWidget {
     TextEditingController controller = TextEditingController();
 
     return Scaffold(
-      body: Center(
-        child: SizedBox(
-          height: 400,
-          width: 400,
+      appBar: AppBar(
+        title: Text("Enter Captcha", style: GoogleFonts.lexend(fontSize: 22)),
+        centerTitle: true,
+        actions: [
+          IconButton(onPressed: () {
+              context.read<ImsProvider>().ims.logout();
+              context.pushReplacement('/authentication/authentication_screen');
+            }, icon: Icon(Icons.logout, color: Theme.of(context).textTheme.bodyLarge!.color,))
+        ],
+      ),
+      body: LoaderOverlay(
+        child: Center(
           child: Column(
-            children: [
-              CaptchImage(ims: ims),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 10),
-                child: TextField(
-                  controller: controller,
-                  decoration: InputDecoration(
-                    labelText: 'Captcha',
-                    filled: true,
-                    fillColor: Theme.of(context).colorScheme.onSecondary,
-                    isDense: true,
-                    prefixIcon: const Icon(Icons.person_rounded),
-                    hintText: "Enter your username",
-                    border: const OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(20))
-                    )
-                  ),),
-              ),
-              ElevatedButton(
-                  onPressed: () async {
-                    tryRelogin(context, ims, controller);
-                  }, 
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Theme.of(context).colorScheme.secondaryContainer,
-                    shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(8)))
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  CaptchImage(ims: ims),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: TextField(
+                      controller: controller,
+                      keyboardType: TextInputType.number,
+                      cursorColor: Theme.of(context).colorScheme.onBackground,
+                      decoration: InputDecoration(
+                        labelText: 'Captcha',
+                        labelStyle: GoogleFonts.lexend(color: Theme.of(context).colorScheme.onBackground),
+                        filled: true,
+                        fillColor: Theme.of(context).colorScheme.primary.withAlpha(150),
+                        isDense: true,
+                        prefixIcon: const Icon(Icons.person_rounded),
+                        hintText: "Fill the digits in captcha above",
+                        hintStyle: GoogleFonts.lexend(color: Theme.of(context).colorScheme.onBackground),
+                        border: const OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(20))
+                        )
+                      ),
                     ),
-                  child: Text("Submit", style: GoogleFonts.habibi(fontSize: 15, fontWeight: FontWeight.w600),),)
-            ],
-          ),
+                  ),
+                  const SizedBox(height: 32,),
+                  ElevatedButton(
+                    onPressed: () async {
+                      tryRelogin(context, ims, controller);
+                    }, 
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Theme.of(context).colorScheme.onBackground,
+                      shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(8)))
+                    ),
+                    child: Text(
+                      "Submit", 
+                      style: GoogleFonts.lexend(fontSize: 15, fontWeight: FontWeight.w600, color: Theme.of(context).colorScheme.primary)
+                    )
+                  )
+                ],
+              ),
         ),
       ),
-    );
+      );
   }
 }
 
@@ -83,7 +108,6 @@ class CaptchImage extends StatelessWidget {
 
   Future<File> getLocalCaptchaImageFile() async {
     String filePath = await Functions.downloadFile(await ims.getCaptcha());
-
     return File(filePath);
   }
 
@@ -96,7 +120,7 @@ class CaptchImage extends StatelessWidget {
 
         if (snapshot.hasData) {
         
-          return Image.file(snapshot.data!);
+          return Image.file(snapshot.data!, height: 100, width: 100, scale: 0.1,);
         
         } else {
           return const Text('Loading....');
