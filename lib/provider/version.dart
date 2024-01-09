dab82dee01af814fbd643dd7bb4913fdf0ccf074
ahmedazim7804 <ahmedazim7804@gmail.com>
2024-01-09 14:29:55 +0530

    added update screen

--- lib/provider/version.dart
+++ lib/provider/version.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart' as http;
 
 class VersionProvider extends ChangeNotifier {
   String currentVersion = '1.0.0';
-  bool needUpdate = true;
+  bool needUpdate = false;
 
   Future<void> isLatestVersion() async {
     final response = await http.get(Uri.parse('https://gist.githubusercontent.com/Ahmedazim7804/6ef4d41859a1fadea8a0f563de9a90dc/raw/183cbb21dbc18f886dcba13e40fcafc56c9bbe0f/gistfile1.txt'));
---
+++ lib/widgets/update_screen.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:imsnsit/provider/version.dart';
+import 'package:provider/provider.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class UpdateScreen extends StatelessWidget {
+  const UpdateScreen({super.key});
+
+  Future<bool> checkForUpdate(VersionProvider versionProvider) async {
+    await versionProvider.isLatestVersion();
+
+    return versionProvider.needUpdate;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+      if (context.read<VersionProvider>().needUpdate) {
+        showDialog(
+          context: context,
+          builder: (_) => const UpdateDialog());
+      } else {
+        context.go('/authentication/authentication_screen');
+      }
+    });
+
+    return const Scaffold(
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text('Checking for update... '),
+            CircularProgressIndicator()
+          ],
+        ),
+      )
+    );
+  }
+}
+
+class UpdateDialog extends StatelessWidget {
+  const UpdateDialog({super.key});
+
+  void goToReleasePage() async {
+    final Uri url = Uri.parse('https://github.com/ahmedazim7804/imsnsit_app');
+
+    if (!await launchUrl(url)) {
+      throw Exception('Could not launch $url');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+  return AlertDialog(
+    title: const Text("Update Available"),
+    content: const Text('A new version of this app is available.\nWould you like to update it now?'),
+    actions: [  
+      TextButton(onPressed: () {
+        goToReleasePage();
+        context.go('/authentication/authentication_screen');
+      }, child: const Text("UPDATE NOW")),
+      TextButton(onPressed: () => context.go('/authentication/authentication_screen'), child: const Text("LATER"))
+    ],
+  );
+  }
+}
