7cefce7ce0c380e9f452e3ce585177d17eecfa57
ahmedazim7804 <ahmedazim7804@gmail.com>
2024-01-09 14:32:37 +0530

    updated url

--- lib/provider/version.dart
+++ lib/provider/version.dart
@@ -6,7 +6,7 @@ class VersionProvider extends ChangeNotifier {
   bool needUpdate = false;
 
   Future<void> isLatestVersion() async {
-    final response = await http.get(Uri.parse('https://gist.githubusercontent.com/Ahmedazim7804/6ef4d41859a1fadea8a0f563de9a90dc/raw/183cbb21dbc18f886dcba13e40fcafc56c9bbe0f/gistfile1.txt'));
+    final response = await http.get(Uri.parse('https://gist.githubusercontent.com/Ahmedazim7804/6ef4d41859a1fadea8a0f563de9a90dc/raw/001fda9648ca4ec50adeaf4bdfeb3ef5162e6441/imsnsit_app_version.txt'));
     String? version = response.body;
     
     if (version != currentVersion) {
