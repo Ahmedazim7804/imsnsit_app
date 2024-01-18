19b25b7f0c1a2de817f1316f8de40e75114d6a0c
ahmedazim7804 <ahmedazim7804@gmail.com>
2024-01-18 21:22:40 +0530

    changes to update dialog

--- lib/widgets/update_screen.dart
+++ lib/widgets/update_screen.dart
@@ -9,29 +9,25 @@ class UpdateScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    
     WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
       if (context.read<VersionProvider>().needUpdate) {
-        showDialog(
-          context: context,
-          builder: (_) => const UpdateDialog());
+        showDialog(context: context, builder: (_) => const UpdateDialog());
       } else {
         context.go('/authentication/authentication_screen');
       }
     });
 
     return const Scaffold(
-      body: Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text('Checking for update... '),
-            CircularProgressIndicator()
-          ],
-        ),
-      )
-    );
+        body: Center(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text('Checking for update... '),
+          CircularProgressIndicator()
+        ],
+      ),
+    ));
   }
 }
 
@@ -39,7 +35,8 @@ class UpdateDialog extends StatelessWidget {
   const UpdateDialog({super.key});
 
   void goToReleasePage() async {
-    final Uri url = Uri.parse('https://github.com/ahmedazim7804/imsnsit_app/releases/tag/latest');
+    final Uri url = Uri.parse(
+        'https://github.com/ahmedazim7804/imsnsit_app/releases/tag/latest');
 
     if (!await launchUrl(url)) {
       throw Exception('Could not launch $url');
@@ -48,16 +45,24 @@ class UpdateDialog extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-  return AlertDialog(
-    title: const Text("Update Available"),
-    content: const Text('A new version of this app is available.\nWould you like to update it now?'),
-    actions: [  
-      TextButton(onPressed: () {
-        goToReleasePage();
-        context.go('/authentication/authentication_screen');
-      }, child: const Text("UPDATE NOW")),
-      TextButton(onPressed: () => context.go('/authentication/authentication_screen'), child: const Text("LATER"))
-    ],
-  );
+    return AlertDialog(
+      title: const Text("Update Available"),
+      content: const Text(
+          'A new version of this app is available.\nWould you like to update it now?'),
+      actions: [
+        TextButton(
+            onPressed: () {
+              goToReleasePage();
+              context.go('/authentication/authentication_screen');
+            },
+            child: const Text("UPDATE NOW")),
+        TextButton(
+            onPressed: () {
+              context.pop();
+              context.go('/authentication/authentication_screen');
+            },
+            child: const Text("LATER"))
+      ],
+    );
   }
 }
