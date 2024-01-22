a58e61cf0ca27cc76827e351ead10eff9b2b9516
ahmedazim7804 <ahmedazim7804@gmail.com>
2024-01-22 16:03:25 +0530

    table parser

---
+++ lib/model/teacher.dart
@@ -0,0 +1,8 @@
+class Teacher {
+  const Teacher(
+      {required this.tutor, required this.tutorCode, required this.subject});
+
+  final String tutor;
+  final String tutorCode;
+  final String subject;
+}
